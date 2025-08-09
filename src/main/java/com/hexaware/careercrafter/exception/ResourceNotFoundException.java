e150d22ab1ae2da58c2f1dff3c00bd25743ec081
Pudiparthi Sutej Reddy <sutejreddy7@gmail.com>
2025-08-09 17:30:27 +0530

    Exceptions

--- src/main/java/com/hexaware/careercrafter/exception/ResourceNotFoundException.java
+++ src/main/java/com/hexaware/careercrafter/exception/ResourceNotFoundException.java
@@ -2,9 +2,6 @@ package com.hexaware.careercrafter.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
     
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public ResourceNotFoundException(String message) {
