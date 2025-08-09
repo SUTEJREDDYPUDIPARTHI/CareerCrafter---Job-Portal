10d4901576f331d501fdac8de958402362d37775
Pudiparthi Sutej Reddy <sutejreddy7@gmail.com>
2025-08-09 17:30:05 +0530

    Exceptions

--- src/main/java/com/hexaware/careercrafter/exception/InvalidRequestException.java
+++ src/main/java/com/hexaware/careercrafter/exception/InvalidRequestException.java
@@ -1,9 +1,7 @@
 package com.hexaware.careercrafter.exception;
 
 public class InvalidRequestException extends RuntimeException {
-    /**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 
 	public InvalidRequestException(String message) {
